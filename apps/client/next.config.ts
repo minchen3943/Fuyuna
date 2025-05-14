a57571180f6885cb8b925d0347aa265d396a4d48
瞑尘 <minchen3943@gmail.com>
2025-05-15 00:07:32 +0800

    Refactor code structure for improved readability and maintainability

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   compiler: {
     styledComponents: true,
   },
-  output: "standalone",
+  output: 'standalone',
 };
 
 export default nextConfig;
